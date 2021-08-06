3c261da5fe069ef6a87c4cb0fb1ff5742654f6eb
Rasmus Rynell <rynell.rasmus@gmail.com>
2021-08-07 00:12:59 +0200

    testing

--- pred_games/analysis.py
+++ pred_games/analysis.py
@@ -12,6 +12,8 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
 
+tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.333, allow_growth=True)
+
 
 dfs = []
 for i in range(2010, 2021):
@@ -42,15 +44,20 @@ df.to_csv('./stats/df.csv', sep=';', index=False)
 def baseline_model():
     # create model
     model = Sequential()
-    model.add(Dense(500, input_dim=432, activation='LeakyReLU'))
-    model.add(Dense(250, input_dim=500, activation='LeakyReLU'))
-    model.add(Dense(124, input_dim=250, activation='LeakyReLU'))
+    model.add(Dense(500, input_dim=432, activation='relu'))
+    model.add(Dense(250, activation='relu'))
+    model.add(Dense(124, activation='relu'))
     model.add(Dense(3, activation='softmax'))
     # Compile model
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
-estimator = KerasClassifier(build_fn=baseline_model, epochs=200, batch_size=200, verbose=0)
-kfold = KFold(n_splits=10, shuffle=True)
-results = cross_val_score(estimator, X, y, cv=kfold)
-print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+####
+model = baseline_model()
+model.fit(X, y, epochs=15, batch_size=50, validation_split=0.2, verbose=2)
+####
+# estimator = KerasClassifier(build_fn=baseline_model, epochs=20, batch_size=500, verbose=0)
+# kfold = KFold(n_splits=10, shuffle=True)
+# results = cross_val_score(estimator, X, y, cv=kfold)
+# print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+####
